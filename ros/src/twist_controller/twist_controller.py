58fecd73c7721802e348c5dbf228d275600ca8fd
Gianni Jacklone <gianni.jacklone@gmail.com>
2017-08-29 10:33:54 -0700

    Use scipy spline for waypoint interpolation. Set max and min steering values for steering PID controller. Changing lanes still causes the car to drive off the road.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import rospy
+from scipy import interpolate
 
 from yaw_controller import YawController
 from pid import PID
@@ -13,10 +14,11 @@ class TwistController(object):
 
         # TODO: Implement
 
-        self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+        # self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
 
-        # self.pid_steering_controller = PID(.5, .0001, 4)
-        self.pid_steering_controller = PID(0.071769, 0.00411344, 0.974954)
+        # self.pid_steering_controller = PID(.5, .0001, 4, -max_steer_angle, max_steer_angle)
+
+        self.pid_steering_controller = PID(0.071769, 0.00411344, 0.974954, -max_steer_angle, max_steer_angle)
 
     def control(self, current_velocity, proposed_velocity, final_waypoints, current_pose):
         # TODO: Change the arg, kwarg list to suit your needs
@@ -33,33 +35,29 @@ class TwistController(object):
 
         y_vals = list(map(lambda wp: wp.pose.pose.position.y, final_waypoints.waypoints))
 
-        # fit 3rd degree poly
-        z = np.polyfit(x_vals, y_vals, 3)
-        p = np.poly1d(z)
-        # current_car_y = current_pose.pose.position.y
-        # target_y = p(x_vals[0])
-        # cross_track_error = target_y - current_car_y
-
         current_car_x = current_pose.pose.position.x
         current_car_y = current_pose.pose.position.y
-        target_y = p(current_car_x)
+
+        # fit 3rd degree poly
+        # z = np.polyfit(x_vals, y_vals, 3)
+        # p = np.poly1d(z)
+        # target_y = p(current_car_x)
+        # cross_track_error = target_y - current_car_y
+
+        # fit a spline
+        tck = interpolate.splrep(x_vals, y_vals)
+        target_y = interpolate.splev(current_car_x, tck, der=0)
         cross_track_error = target_y - current_car_y
+        rospy.logwarn('cross_track_error: %s', cross_track_error)
 
-        if (abs(cross_track_error) > 0.3):
-            rospy.logwarn('cross_track_error: %s', cross_track_error)
-            self.pid_steering_controller.reset()
-
-        # current_car_x = current_pose.pose.position.x
-        # target_y = y_vals[0]
-        # car_fitted_y = p(current_car_x)
-        # cross_track_error = target_y - car_fitted_y
-
-
+        # if (abs(cross_track_error) > 0.3):
+            # self.pid_steering_controller.reset()
 
         sample_time = .02 # ??? Match Rate in dbw_node?
         # sample_time = 1
 
         steer = self.pid_steering_controller.step(cross_track_error, sample_time)
+        rospy.logwarn('steer: %s', steer)
 
         throttle = 10
         brake = 0
