f8c09547614f6b42962b814c562bba0e9af8a3a0
binliu-base <2598725694@qq.com>
2017-09-02 22:44:21 +0800

    add bag processing code (traffic light image), under progress

---
+++ test/bag_topic_def.py
@@ -0,0 +1,10 @@
+""" ROS Bag file topic definitions
+"""
+
+SINGLE_CAMERA_TOPIC = "/camera/image_raw"
+CAMERA_TOPICS = [SINGLE_CAMERA_TOPIC]
+
+TF_LIGHTS_TOPIC="/vehicle/traffic_lights"
+CAR_POSE_TOPIC="/current_pose"
+
+
